3195277d2c2b96128cb466f1ea1f0b7fee57e523
gautam-ch <147068398+gautam-ch@users.noreply.github.com>
2026-01-11 22:42:04 +0530

    added sponsor page

---
+++ app/sponsors/page.jsx
@@ -0,0 +1,9 @@
+import Sponsors from "../../components/sections/Sponsors";
+
+export default function SponsorsPage() {
+  return (
+    <div className="pt-20">
+      <Sponsors />
+    </div>
+  )
+}
